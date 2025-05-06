a7a1f8fececb24e1e7cb601701199d11f69580bf
Jean <jeanalbalouh@gmail.com>
2025-05-06 16:05:07 +0200

    feature/update-layouts

---
+++ frontend/src/layout/CustomerLayout.jsx
@@ -0,0 +1,31 @@
+// React
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+
+// Auth & Theme context
+import { ThemeContext } from "../context/Context";
+
+// Layout components
+import CustomerHeader from "./header/CustomerHeader";
+
+// Third
+import { useTranslation } from "react-i18next";
+
+const CustomerLayout = () => {
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    // "en" needs to be replaced by a dynamic variable that is toggled by user interaction, reload page when clicked
+    i18n.changeLanguage("it");
+  }, [i18n]);
+  return (
+    <ThemeContext.Provider value={null}>
+      <CustomerHeader />
+      <main>
+        <Outlet />
+      </main>
+    </ThemeContext.Provider>
+      );
+};
+
+export default CustomerLayout;
---
+++ frontend/src/layout/DelivrerLayout.jsx
@@ -0,0 +1,33 @@
+// React
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+
+// Auth & Theme context
+import { ThemeContext } from "../context/Context";
+
+// Layout components
+import DelivrerHeader from "./header/DelivrerHeader";
+
+// Third
+import { useTranslation } from "react-i18next";
+
+const DelivrerLayout = () => {
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    // "en" needs to be replaced by a dynamic variable that is toggled by user interaction, reload page when clicked
+    i18n.changeLanguage("it");
+  }, [i18n]);
+  return (
+    <div className="flex flex-row bg-zinc-900">
+      <ThemeContext.Provider value={null}>
+        <DelivrerHeader />
+        <main>
+          <Outlet />
+        </main>
+      </ThemeContext.Provider>
+    </div>
+  );
+};
+
+export default DelivrerLayout;
