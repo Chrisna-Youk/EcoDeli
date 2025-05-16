41670804f6f88b9feb506f0f5d307b1ac1956fc4
Chrisna Youk <cyouk@myges.com>
2025-05-16 16:26:46 +0200

    [Components] adding component DisplayProvider

--- frontend/src/routes/PublicRoutes.jsx
+++ frontend/src/routes/PublicRoutes.jsx
@@ -6,10 +6,12 @@ import PublicLayout from "../layout/PublicLayout";
 
 // Pages
 import HomePublic from "../pages/public/HomePublic";
+import DisplayProvider from "../pages/TO_IMPLEMENT/DisplayProvider";
 
 const PublicRoutes = (
   <Route path="/" element={<PublicLayout />}>
     <Route index element={<HomePublic />} />
+    <Route path="display-cards" element={<DisplayProvider />}/>
   </Route>
 );
 
