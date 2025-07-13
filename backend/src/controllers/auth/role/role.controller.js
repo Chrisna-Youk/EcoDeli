80d0544c7d4b5ee2fc896b90e94e67a4bcc007ab
jean-albalouh <jeanalbalouh@gmail.com>
2025-07-13 20:57:51 +0200

    fix/minibugs-routes-endpoints

--- backend/src/controllers/auth/role/role.controller.js
+++ backend/src/controllers/auth/role/role.controller.js
@@ -3,8 +3,12 @@ const { decode } = jwt;
 
 async function roleController(req, res) {
   const { refreshToken } = req.cookies;
-  const role = decode(refreshToken, process.env.REFRESH_TOKEN_KEY).role;
-  res.status(200).json({ data: role });
+  try {
+    const role = decode(refreshToken, process.env.REFRESH_TOKEN_KEY).role;
+    res.status(200).json({ data: role });
+  } catch (error) {
+    res.status(500).json({ data: "Erreur serveur" });
+  }
 }
 
 export default roleController;
