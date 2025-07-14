697ee97fa2e7973ac1c8ab055210cdd4ddea71b3
777loki <loki.infosec@gmail.com>
2025-07-14 14:46:41 +0200

    component/delivery-detail-page

--- frontend/src/routes/CustomerRoutes.jsx
+++ frontend/src/routes/CustomerRoutes.jsx
@@ -27,6 +27,7 @@ import ProfileDetails from "../pages/customer/ProfileDetails";
 import RequestBecomeDeliverer from "../pages/customer/RequestBecomeDelivrer";
 import RequestBecomeProvider from "../pages/customer/RequestBecomeProvider";
 import TransportDetailsPage from "../pages/customer/TransportDetailsPage";
+import DeliveryDetailsPage from "../pages/TO_IMPLEMENT/test";
 
 const CustomerRoutes = (
   <>
