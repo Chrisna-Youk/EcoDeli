af3574a6374e0f7edae7f4aec82101d5c98ede8f
jean-albalouh <jeanalbalouh@gmail.com>
2025-07-16 14:21:06 +0200

    feature/first-steps-into-business-logic-of-delivery

--- backend/src/controllers/announcement/read/readById.controller.js
+++ backend/src/controllers/announcement/read/readById.controller.js
@@ -1,17 +1,32 @@
 import Announcement from "../../../models/announcement.model.js";
+import Category from "../../../models/category.model.js";
+import User from "../../../models/user.model.js";
 
 async function readAnnouncementByIdController(req, res) {
-  const { id } = req.query;
+  const { announcementId } = req.params;
 
   try {
-    const annoucement = await Announcement.findAll({
+    const annoucement = await Announcement.findOne({
       where: {
-        id: id,
+        id: announcementId,
       },
+      include: [
+        {
+          model: User,
+          as: "user",
+          attributes: ["id", "photo", "role", "firstName", "lastName"],
+        },
+        {
+          model: Category,
+          as: "category",
+          attributes: ["id", "name"],
+        },
+      ],
     });
-    return res.status(200).json({ message: annoucement });
+    return res.status(200).json({ data: annoucement });
   } catch (error) {
-    return res.status(400).json({ message: "Bad request" });
+    console.log(error);
+    return res.status(500).json({ data: "Erreur server" });
   }
 }
 
